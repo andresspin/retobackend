5df1ee5ac6cb0a7132f4adbac922f1cf241fcf14
andresspin <andresspin80@gmail.com>
2022-12-19 18:17:33 -0500

    corregido

--- app.js
+++ app.js
@@ -1,38 +1,35 @@
-const express = require('express')
-const app = express()
-const port = 8080
-
-app.use(express.json())
-
+const express = require("express");
+const app = express();
+const port = 8080;
 let students = {
+  top: {
     version_top: 26,
     students: 10,
     names: [
-        "Felipe", 
-        "Laura", 
-        "Jailer", 
-        "Jaime", 
-        "Juan", 
-        "Andrés",
-        "Nicolas",
-        "Carlos",
-        "Ivan",
-        "Diana",
-        "Eustaquio",
-        "Hermeregildo"
-    ]
-}
-
-app.post('/My-first-endpoint', (req, res) => {
-    const {}
-
-    if(!students) {
-        res.status(200).json({message: "Curso encontrado satisfactoriamente", top: students })
-    } else {
-        res.status(400).json({message: "Curso no encontrado"})
-    }
-})
+      "Felipe",
+      "Laura",
+      "Jailer",
+      "Jaime",
+      "Juan",
+      "Andrés",
+      "Nicolas",
+      "Carlos",
+      "Ivan",
+      "Diana",
+    ],
+  },
+};
+app.get("/My-first-endpoint", (req, res) => {
+  res.status(200).json({
+    message: "Curso encontrado satisfactoriamente",
+    students: students,
+  });
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
